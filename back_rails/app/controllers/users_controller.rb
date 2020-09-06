69ffa6fe70ec88e5f705d16848b5266560996357
minamihiroto <kokowatamf@gmail.com>
2020-09-06 21:19:56 +0900

    🔖Update controller add crud

--- back_rails/app/controllers/users_controller.rb
+++ back_rails/app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
   def index
-     @users = User.where(id: params[:id])
+    @user = User.all
+    render :json => @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :json => @user
+  end
+  
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    @user.save
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :image_url, :sns_url_fb, :sns_url_tw, :prafile)
   end
 end
