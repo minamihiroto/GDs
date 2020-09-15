1f3b9171ecd45df17a894275863a03c62afb0b5a
minamihiroto <kokowatamf@gmail.com>
2020-09-15 15:41:52 +0900

    🔖 Update users_controller.rb

--- back_rails/app/controllers/users_controller.rb
+++ back_rails/app/controllers/users_controller.rb
@@ -1,28 +1,34 @@
 class UsersController < ApplicationController
+
+protect_from_forgery
+
   def index
     @user = User.all
-    render :json => @user
+    render json: @user
   end
 
   def show
     @user = User.find(params[:id])
-    render :json => @user
+    render json: @user
   end
   
   def create
     @user = User.new(user_params)
     @user.save
+    render json: @user
   end
   
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     @user.save
+    render json: @user
   end
   
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    render json: @user
   end
 
   private
