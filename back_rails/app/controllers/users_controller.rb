7280de040eb7a62efe18e9a15cd96bdd914b0e8d
minamihiroto <kokowatamf@gmail.com>
2020-09-18 20:01:20 +0900

    🔖Update controller add error-message

--- back_rails/app/controllers/users_controller.rb
+++ back_rails/app/controllers/users_controller.rb
@@ -9,20 +9,30 @@ protect_from_forgery
 
   def show
     @user = User.find(params[:id])
-    render json: @user
+    if @user = true
+      render json: @user
+    else
+      puts 'not found'
+    end
   end
   
   def create
     @user = User.new(user_params)
-    @user.save
-    render json: @user
+    if @user.save
+      render json: @user
+    else
+      puts 'error'
+    end
   end
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    @user.save
-    render json: @user
+    if @user.update(user_params)
+      @user.save
+      render json: @user
+    else
+      puts 'error'
+    end
   end
   
   def destroy
