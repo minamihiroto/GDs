2dd8f12e5f313ca670a0fbbe2d852478ba31853d
TakehiroTada <taketada.works.gmail.com>
2020-09-24 19:34:34 +0900

    tada-code

--- front/src/pages/Temp.tsx
+++ front/src/pages/Temp.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent, useEffect } from "react";
-import axios from 'axios';
+import React, { FunctionComponent, useEffect, useState } from "react";
+import axios from "axios";
 
 type User = {
   id: number;
@@ -9,24 +9,34 @@ type User = {
   sns_url_tw: string | null;
   sns_url_fb: string | null;
   profile: string | null;
-}
+};
 
 const Home: FunctionComponent = () => {
-  const getUsers = () => {
-    axios.get('http://localhost:4000/users').then((res) => {
-      console.log(res)
-    })
-    // const result: User[] = axios.get('http://localhost:4000/users')
-    // const result2: User[] = axios.get('http://localhost:4000/users/1')
-  }
+  const [users, setUsers] = useState<User[]>([]); // 状態保持（useState)
+  const getUsers = async () => {
+    // ユーザー一覧をAPIから取得する関数
+    const result = await axios.get("http://localhost:4000/users");
+    setUsers(result.data);
+  };
 
   useEffect(() => {
-    getUsers()
-  }, [])
+    // ページロード時に自動でgetUsers関数を発火（useEffect)
+    getUsers();
+  }, []);
 
   return (
     <div>
-      <span>Aaaa</span>
+      {users.length > 1 &&
+        users.map((user) => {
+          return (
+            <>
+              <span>
+                id: {user.id} name: {user.name}
+              </span>
+              <br />
+            </>
+          );
+        })}
     </div>
   );
 };
